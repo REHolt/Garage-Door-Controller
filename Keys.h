0135fb45cfeab3e6e547a79bc84a5610388f8f48
REHolt <Traveler218@gmail.com>
2016-08-20 14:14:19 -0500

    Update Keys.h

--- Keys.h
+++ Keys.h
@@ -1,5 +1,5 @@
-const char* wifiCreds[] = {"Thunderzone", "Brn2bwld!"};
+const char* wifiCreds[] = {"SSID", "Password"};
 
-char* Host_Name ="Cerberus";
+char* Host_Name ="Garage Door";
 
 String iftttMakerUrl = "https://maker.ifttt.com/trigger/esp_message/with/key/TBD";
